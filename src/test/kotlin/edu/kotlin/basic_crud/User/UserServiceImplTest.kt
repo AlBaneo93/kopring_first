f90ae5e38cf299eea4cb9e6bf6ed4ee01135b380
albaneo93 <albaneo0724@gmail.com>
2022-05-28 11:45:17 +0900

    [+] add userService Tetst code

--- src/test/kotlin/edu/kotlin/basic_crud/User/UserServiceImplTest.kt
+++ src/test/kotlin/edu/kotlin/basic_crud/User/UserServiceImplTest.kt
@@ -1,31 +1,18 @@
 package edu.kotlin.basic_crud.User
 
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+
 import java.time.LocalDateTime
 
-@SpringBootTest
-class UserServiceImplTest @Autowired constructor(private val repository: UserRepository) {
+class UserServiceImplTest(private val repository: UserRepository) {
+
 
   @Test
-  @DisplayName("회원가입 테스트")
   fun signUp() {
-    val user = User(
-      seq = 0,
-      id = "test@email.com",
-      password = "test",
-      name = "test",
-      isAdmin = true,
-      createdAt = LocalDateTime.now(),
-      updatedAt = LocalDateTime.now(),
-      authorities = mutableSetOf(UserRole.USER),
-      joinGroups = mutableSetOf()
+    var user: User = User(
+      0, "eclementss@webeden.co.uk", "1234", "Ester", true, setOf(UserRole.ADMIN), LocalDateTime.now(), LocalDateTime.now()
     )
-    println("before seq : ${user.seq}")
-    repository.save(user)
-    println("after seq : ${user.seq}")
+
   }
 
 }
